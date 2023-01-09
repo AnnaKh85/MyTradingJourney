a6fb831ee89ebc37def76d0f01d6f9b637e105c1
Anna <AKhizchnyakova@1cbit.ru>
2023-01-09 22:41:17 +0300

    new posts
    new posts

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Анна Хижнякова';
+    : 'My trading journey';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Сайт о том как я постигала азы трейдерского искусства';
