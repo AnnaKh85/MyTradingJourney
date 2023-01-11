9ef317e49f1c14d03cfe8191ae5256160f698c94
Anna <AKhizchnyakova@1cbit.ru>
2023-01-11 07:15:31 +0300

    cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require('cypress');
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "spdb6n",
   e2e: {
-    baseUrl: 'http://localhost:3000',
-    supportFile: false,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
   },
 });
